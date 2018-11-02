70caa0a412568b5f1b3e9eae715199f532437ea3
simon_xlg <285909195@qq.com>
2018-11-02 16:45:50 +0800

    fix bug

--- patch.js
+++ patch.js
@@ -1,16 +1,24 @@
 function patch(obj,diff){
-    var merge = function(o,k,v){
+ var merge = function(o,k,v){
             var curr = o;
             var arr = k.split(".")
             var len =  arr.length
-            for (var i = 1; i < len-2; i++) {
-                curr = obj[arr[i]]
+            var key = ""
+            for (var i = 1; i < len-1; i++) {
+                if (isNaN(Number(arr[i]))){
+                    key = arr[i]
+                }else{
+                    key = (Number(arr[i]) -1)
+                }
+                curr = curr[key]
             }
-            var key = arr[len-1]
-            curr[key] = (v == "nil"?null:v)
+            curr[arr[len-1]] = (v == "nil"?null:v)
         }
-    for(var key in diff){
-        merge(obj,key,diff[key])
-    }
-    return obj
+        for (var i =0 ;i<diff.length;i++){
+            for(var key in diff){
+                merge(obj,key,diff[key])
+            }
+        }
+        return obj
 }
+
